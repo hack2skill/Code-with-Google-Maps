91ef92dbc252625a4feb1952a18d33ee3a3f45f1
Tijin Abe Thomas <tijinabet@gmail.com>
2023-10-26 21:48:16 +0530

    updated weighted heatmaps api

--- xaea12/api/index.py
+++ xaea12/api/index.py
@@ -2,9 +2,13 @@ import datetime
 
 import requests
 from flask import Flask, jsonify
+import json
 
 import populartimes
 import googlemaps
+
+import os
+API_KEY = os.getenv("API_KEY")
 
 #app instance
 app = Flask(__name__)
@@ -14,15 +18,50 @@ def hello_world():
     return "<p>Hello, World!</p>"
 
 @app.route("/api/popular_times")
+def weight_gen():
+    weights = []
+    pop_time = popular_times()
+    
+    for index in range(len(pop_time)):
+        pop_time_temp = pop_time[index]
+        weight = {
+            "coordinates": pop_time_temp["coordinates"],
+            "weekly_sum": 0
+        }
+
+        for day_data in pop_time_temp["populartimes"]:
+            data_values = day_data["data"]
+            day_sum = sum(data_values)
+            weight["weekly_sum"] += day_sum
+
+        weights.append(weight)
+
+    return weights
+
+
 def popular_times():
-    gmaps = googlemaps.Client(key='AIzaSyDi9JdwPoacIpGgQKvZCZMyNsKQbuaZu94')
-    query_response = gmaps.places(query="Kollam Railway Station")
-    return query_response
+    [p1, p2] = geocode()
+    response = populartimes.get(API_KEY, ["bar"], p1, p2)
+    return response
+
+def geocode():
+    gmaps = googlemaps.Client(key=API_KEY)
+    geocode_response = gmaps.geocode(address="Surathkal")
+    geocode_response_dict = geocode_response[0]
+    
+    northeast = geocode_response_dict["geometry"]["bounds"]["northeast"]
+    southwest = geocode_response_dict["geometry"]["bounds"]["southwest"]
+
+    p1 = (southwest["lat"], southwest["lng"])
+    p2 = (northeast["lat"], northeast["lng"])
+
+    return [p1, p2]
 
     #Approach 1: We don't have much work in terms of getting popular times here
-    #Syntax of popular times lib: https://github.com/m-wrzr/populartimes#populartimesget
-        # response = populartimes.get(api_key="AIzaSyAHxiCosxz3mxqBlYjtyNxInNKTC8VD-qA", types=["bar"], p1=(48.132986, 11.566126), p2=(48.142199, 11.580047))
-        # return response
+    # #Syntax of popular times lib: https://github.com/m-wrzr/populartimes#populartimesget
+    # response = populartimes.get_id(API_KEY, "ChIJj0i_N0xaozsRZP78dHq8e4I")
+    # response = populartimes.get(API_KEY, ["restaurant"], (12.907582, 74.810978), (12.927127, 74.879116))
+    # return response
     #It is ideally supposed to be giving me a combined popular times data of all such establishments in the region
 
     #Approach 2 
