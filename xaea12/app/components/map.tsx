e4218df101f089d683cd8bef1a90851cdfade780
vedikagadia <vedikagadia02@gmail.com>
2023-10-26 20:43:16 +0530

    heatmap layer added

--- xaea12/app/components/map.tsx
+++ xaea12/app/components/map.tsx
@@ -1,11 +1,15 @@
 import { useState, useMemo, useCallback, useRef } from "react";
 import {
     GoogleMap,
+    HeatmapLayer,
+    HeatmapLayerProps,
+    HeatmapLayerF
 } from "@react-google-maps/api";
 
 type LatLngLiteral = google.maps.LatLngLiteral;
 type DirectionsResult = google.maps.DirectionsResult;
 type MapOptions = google.maps.MapOptions;
+type heatMapOptions = google.maps.visualization.HeatmapLayerOptions
 
 export default function Map() {
     const mapRef = useRef<GoogleMap>();
@@ -13,6 +17,7 @@ export default function Map() {
         () => ({ lat: 43.45, lng: -80.49 }),
         []
     );
+    const onLoad = useCallback((map) => (mapRef.current = map), []);
     const options = useMemo<MapOptions>(
         () => ({
             mapId: "5e7d266a88c0cf9",
@@ -21,30 +26,53 @@ export default function Map() {
         }),
         []
     );
-    const heatMapData = {
-        positions: [
-            { lat: 55.5, lng: 34.56 },
-            { lat: 34.7, lng: 28.4 },
-        ],
-        options: {
-            radius: 20,
-            opacity: 0.6,
-        }
-    }
-    const onLoad = useCallback((map) => (mapRef.current = map), []);
+    const heatMapOptions = useMemo<heatMapOptions>(
+        () => ({
+            gradient: [
+                "rgba(0, 255, 255, 0)",
+                "rgba(0, 255, 255, 1)",
+                "rgba(0, 191, 255, 1)",
+                "rgba(0, 127, 255, 1)",
+                "rgba(0, 63, 255, 1)",
+                "rgba(0, 0, 255, 1)",
+                "rgba(0, 0, 223, 1)",
+                "rgba(0, 0, 191, 1)",
+                "rgba(0, 0, 159, 1)",
+                "rgba(0, 0, 127, 1)",
+                "rgba(63, 0, 91, 1)",
+                "rgba(127, 0, 63, 1)",
+                "rgba(191, 0, 31, 1)",
+                "rgba(255, 0, 0, 1)",
+            ],
+            opacity: 2,
+            radius: 50
+        }),
+        []
+    );
+    var heatMapData = [
+        { location: new google.maps.LatLng(37.782, -122.447), weight: 20 },
+
+    ];
+    var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+
+    // const onLoad = useCallback((map) => (mapRef.current = map), []);
     return (
         <div className="container">
             <div className="controls"><h1>Business Type?</h1></div>
-            <div className="map">
+            <div id="map" className="map">
                 <GoogleMap
                     zoom={10}
-                    center={center}
-                    heatmapLibrary={true}
-                    heatmap={heatMapData}
+                    center={sanFrancisco}
                     mapContainerClassName="map-container"
                     options={options}
                     onLoad={onLoad}
-                ></GoogleMap>
+                >
+                    <HeatmapLayerF
+                        data={heatMapData}
+                        options={heatMapOptions}
+
+                    />
+                </GoogleMap>
             </div>
         </div>
     )
