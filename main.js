08b2ebd90272225272633a3fb6ad06ddeea131a0
JunaTeresMartin <83876430+JunaTeresMartin@users.noreply.github.com>
2023-10-29 21:41:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+// Initialize and add the map
+let map;
+
+async function initMap() {
+  // The location of Uluru
+  const position = { lat: -25.344, lng: 131.031 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+
+  // Define a map style for dark mode
+  const darkModeStyle = [
+    {
+      featureType: "all",
+      elementType: "labels.text.fill",
+      stylers: [{ color: "#ffffff" }],
+    },
+    // Add more style configurations as needed.
+  ];
+
+
+  // The map, centered at Uluru
+  map = new Map(document.getElementById("map"), {
+    zoom: 4,
+    center: position,
+    mapId: "DEMO_MAP_ID",
+    styles: darkModeStyle,
+  });
+
+  // The marker, positioned at Uluru
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Uluru",
+  });
+}
+
+initMap();
+
