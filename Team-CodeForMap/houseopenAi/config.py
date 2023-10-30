4a59a11054d1e5e11b64dc082cb755517d24ea6b
Ankush kumar kothiyal <82761600+01ankush@users.noreply.github.com>
2023-10-30 13:16:42 +0530

    Update config.py

--- Team-CodeForMap/houseopenAi/config.py
+++ Team-CodeForMap/houseopenAi/config.py
@@ -1,1 +1,2 @@
-apikey = "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"
+# Create a new account on open ai and you will get free $5 for api key ,generate one and paste that here..
+apikey = "your api key"
